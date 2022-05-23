98a4d8c62315d4fabb04ba3f20ab8ee58d54ba2d
guseynov <rslnguseynov@gmail.com>
2022-05-23 22:24:42 +0300

    simplify db, add caching, add restoring cache, add writing to db

--- db.sql
+++ db.sql
@@ -1,77 +1,143 @@
-createdb -p 5433 (5432) wb
+-- createdb -p 5433 (5432) wb
 
-CREATE USER wb_user with encrypted password 'wb_pass';
-GRANT ALL PRIVILEGES ON DATABASE wb to wb_user;
+-- CREATE USER wb_user with encrypted password 'wb_pass';
+-- GRANT ALL PRIVILEGES ON DATABASE wb to wb_user;
 
-CREATE TABLE payment (
+CREATE TABLE "orders" (
     id SERIAL PRIMARY KEY,
-    transaction VARCHAR(50) UNIQUE,
-    request_id VARCHAR(20),
-    currency CHAR(3) NOT NULL,
-    provider VARCHAR(20) NOT NULL,
-    amount INT NOT NULL,
-    payment_dt INT NOT NULL,
-    bank VARCHAR(50) NOT NULL,
-    delivery_cost INT,
-    goods_total INT,
-    custom_fee INT
+    uid VARCHAR(20) UNIQUE NOT NULL,
+    data TEXT NOT NULL
 );
 
-CREATE TABLE item (
-    id SERIAL PRIMARY KEY,
-    chrt_id INT,
-    track_number VARCHAR(20),
-    price INT NOT NULL,
-    rid VARCHAR(50),
-    name VARCHAR(50) NOT NULL,
-    sale INT,
-    size VARCHAR(10),
-    total_price INT,
-    nm_id INT,
-    brand VARCHAR(50),
-    status INT
-);
+-- CREATE TABLE payment (
+--     id SERIAL PRIMARY KEY,
+--     transaction VARCHAR(50) UNIQUE,
+--     request_id VARCHAR(20),
+--     currency CHAR(3) NOT NULL,
+--     provider VARCHAR(20) NOT NULL,
+--     amount INT NOT NULL,
+--     payment_dt INT NOT NULL,
+--     bank VARCHAR(50) NOT NULL,
+--     delivery_cost INT,
+--     goods_total INT,
+--     custom_fee INT
+-- );
 
-CREATE TABLE delivery (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    phone VARCHAR(12) NOT NULL,
-    zip VARCHAR(10) NOT NULL,
-    city VARCHAR(50) NOT NULL,
-    address VARCHAR(50) NOT NULL,
-    region VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL
-);
+-- CREATE TABLE item (
+--     id SERIAL PRIMARY KEY,
+--     chrt_id INT,
+--     track_number VARCHAR(20),
+--     price INT NOT NULL,
+--     rid VARCHAR(50),
+--     name VARCHAR(50) NOT NULL,
+--     sale INT,
+--     size VARCHAR(10),
+--     total_price INT,
+--     nm_id INT,
+--     brand VARCHAR(50),
+--     status INT
+-- );
 
-CREATE TABLE order (
-    id SERIAL PRIMARY KEY,
-    order_uid VARCHAR(50) UNIQUE NOT NULL,
-    track_number VARCHAR(20) NOT NULL,
-    entry VARCHAR(10),
-    delivery_id INT NOT NULL,
-    payment_id INT NOT NULL,
-    items INT[],
-    CONSTRAINT fk_delivery
-        FOREIGN KEY(delivery_id) 
-	    REFERENCES delivery(id),
-    CONSTRAINT fk_payment,
-        FOREIGN KEY(payment_id),
-        REFERENCES payment(id),
-    locale VARCHAR(10) NOT NULL,
-    internal_signature VARCHAR(20),
-    customer_id VARCHAR(50) NOT NULL,
-    delivery_service VARCHAR(20) NOT NULL,
-    shardkey VARCHAR(10),
-    sm_id INT,
-    date_created TIMESTAMPTZ NOT NULL,
-    oof_shard VARCHAR(10)
-);
+-- CREATE TABLE delivery (
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR(50) NOT NULL,
+--     phone VARCHAR(12) NOT NULL,
+--     zip VARCHAR(10) NOT NULL,
+--     city VARCHAR(50) NOT NULL,
+--     address VARCHAR(50) NOT NULL,
+--     region VARCHAR(50) NOT NULL,
+--     email VARCHAR(50) NOT NULL
+-- );
 
-CREATE TABLE timetest (
-    date TIMESTAMPTZ NOT NULL
-);
+-- CREATE TABLE "order" (
+--     id SERIAL PRIMARY KEY,
+--     order_uid VARCHAR(50) UNIQUE NOT NULL,
+--     track_number VARCHAR(20) NOT NULL,
+--     entry VARCHAR(10),
+--     delivery_id INT NOT NULL,
+--     payment_id INT NOT NULL,
+--     locale VARCHAR(10) NOT NULL,
+--     internal_signature VARCHAR(20),
+--     customer_id VARCHAR(50) NOT NULL,
+--     delivery_service VARCHAR(20) NOT NULL,
+--     shardkey VARCHAR(10),
+--     sm_id INT,
+--     date_created TIMESTAMPTZ NOT NULL,
+--     oof_shard VARCHAR(10),
+--     CONSTRAINT fk_delivery
+--         FOREIGN KEY(delivery_id) 
+--             REFERENCES delivery(id),
+--     CONSTRAINT fk_payment
+--         FOREIGN KEY(payment_id)
+--             REFERENCES payment(id)
+-- );
 
-INSERT INTO timetest (date) VALUES ('2021-11-26T06:22:19Z');
+-- CREATE TABLE order_item (
+--     order_id INT,
+--     item_id INT,
+--     PRIMARY KEY(order_id, item_id),
+--     CONSTRAINT fk_order
+--         FOREIGN KEY(order_id)
+--             REFERENCES "order"(id),
+--     CONSTRAINT fk_item
+--         FOREIGN KEY(item_id)
+--             REFERENCES item(id)
+-- );
 
-SELECT to_char(date, 'YYYY-MM-DD"T"HH:MI:SS TZ') FROM timetest;
-SELECT to_char(date AT TIME ZONE 'z', 'YYYY-MM-DD"T"HH:MI:SSZ') FROM timetest;
+-- CREATE VIEW everything AS
+-- SELECT
+--     order_uid, "order".track_number, entry,
+--     delivery.name as del_name, phone, zip, city, address, region, email, -- delivery
+--     transaction, request_id, currency, provider, amount, -- payment
+--     payment_dt, bank, delivery_cost, goods_total, custom_fee, -- payment
+--     chrt_id, item.track_number as item_tn, price, rid, -- item                  
+--     item.name as item_name, sale, size, total_price, nm_id, brand, status -- item
+--     locale, internal_signature, customer_id, delivery_service, shardkey,
+--     sm_id, date_created, oof_shard 
+-- FROM order_item
+-- JOIN "order"
+--     ON "order".id = order_item.order_id
+-- JOIN delivery
+--     ON "order".delivery_id = delivery.id
+-- JOIN payment
+--     ON "order".payment_id = payment.id
+-- JOIN item
+--     ON order_item.item_id = item.id;
+
+-- CREATE TABLE timetest (
+--     date TIMESTAMPTZ NOT NULL
+-- );
+
+-- INSERT INTO timetest (date) VALUES ('2021-11-26T06:22:19Z');
+
+-- SELECT to_char(date, 'YYYY-MM-DD"T"HH:MI:SS TZ') FROM timetest;
+-- SELECT to_char(date AT TIME ZONE 'z', 'YYYY-MM-DD"T"HH:MI:SSZ') FROM timetest;
+
+-- INSERT INTO item (chrt_id, track_number, price, rid, name, sale, size, 
+--             total_price, nm_id, brand, status)
+--     VALUES (9934930, 'WBLILTRACK', 453, 'ab6423nbdf94test', 'Redhat',
+--             30, 0, 317, 23454, 'hm', 202);
+
+-- INSERT INTO item (chrt_id, track_number, price, rid, name, sale, size, 
+--             total_price, nm_id, brand, status)
+--     VALUES (123, 'WBLSDFTRACK', 123, 'shadfas932', 'blackhat',
+--             30, 0, 317, 23454, 'hm', 202);
+
+-- INSERT INTO payment (transaction, request_id, currency, provider, amount,
+--             payment_dt, bank, delivery_cost, goods_total, custom_fee)
+--     VALUES ('b56feb67bfc', '', 'USD', 'wbpay', '1817', 163790233, 'alpha',
+--             1500, 317, 0);
+
+-- INSERT INTO delivery (name, phone, zip, city, address, region, email)
+--     VALUES ('Test Testov', '+9720000000', '2639809', 'Kiryat Mozkin',
+--             'Ploshad Mira 15', 'Kraiot', 'test@gmail.com');
+
+-- INSERT INTO "order" (order_uid, track_number, entry, delivery_id, payment_id,
+--                     locale, internal_signature, customer_id, delivery_service,
+--                     shardkey, sm_id, date_created, oof_shard)
+--     VALUES ('b563feb7b2b84b6test', 'WBILMTESTTRACK', 'WBIL', 1, 1, 'en', '',
+--             'test', 'meest', '9', 99, '2021-11-26T06:22:19Z', '1');
+
+-- INSERT INTO order_item VALUES (1, 1);
+
+
--- main.go
+++ main.go
@@ -2,29 +2,37 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os/user"
+	"time"
 
 	stan "github.com/nats-io/stan.go"
 
 	_ "github.com/lib/pq"
 )
 
+const (
+	durName = "orders_L0"
+)
+
 var (
-	db *sql.DB
-	sc stan.Conn
+	db		*sql.DB
+	sc		stan.Conn
+	cache	map[string]Order
 )
 
 func init() {
+	cache = make(map[string]Order)
 	port := 5432
 	u, err := user.Current()
-	if err != nil && u.Username == "dgidget" {
+	if err == nil && u.Username == "dgidget" {
 		port = 5433 //for running at school
 	}
 	connStr := fmt.Sprintf(
 		"postgres://wb_user:wb_pass@localhost:%d/wb?sslmode=disable", port)
-	db, err := sql.Open("postgres", connStr)
+	db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -35,8 +43,76 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	restoreCache()
 }
 
 func main() {
+	defer db.Close()
 
+	ins, err := db.Prepare("INSERT INTO orders (uid, data) values ($1, $2);")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ins.Close()
+	
+	upd, err := db.Prepare("UPDATE orders SET data = $2 WHERE uid = $1;")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer upd.Close()
+
+	defer sc.Close()
+	sub, err := sc.Subscribe("orders", func (m *stan.Msg) {
+		handleMessage(m, ins, upd)
+	}, stan.DurableName(durName))
+	defer sub.Unsubscribe()
+
+	time.Sleep(10 * time.Minute)
 }
+
+func restoreCache() {
+	sel, err := db.Prepare("SELECT * FROM orders;")
+	if err != nil {
+		log.Fatal(err)
+	}
+	rows, err := sel.Query()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var id int
+	var uid, raw []byte
+	var newOrder Order
+	for rows.Next() {
+		if err := rows.Scan(&id, &uid, &raw); err != nil {
+			log.Fatal(err)
+		}
+		if err := json.Unmarshal(raw, &newOrder); err != nil {
+			log.Fatal(err)
+		}
+		cache[newOrder.OrderUID] = newOrder
+	}
+}
+
+func handleMessage(m *stan.Msg, ins, upd *sql.Stmt) {
+	var newOrder Order;
+
+	if err := json.Unmarshal(m.Data, &newOrder); err != nil {
+		log.Println("Received invalid JSON data")
+		return
+	}
+	_, exists := cache[newOrder.OrderUID]
+	cache[newOrder.OrderUID] = newOrder;
+	log.Printf("Received a new order: %s\n", newOrder.OrderUID)
+
+	var op *sql.Stmt
+	if exists {
+		op = upd
+	} else {
+		op = ins
+	}
+	if _, err := op.Exec(newOrder.OrderUID, string(m.Data)); err != nil {
+		log.Fatal(err)
+	}
+}
+
+
